a5cd0f2a3aa0ca0377209c0781b77486b5cf98b4
Jorge Luis <mejiajimenez@gmail.com>
2022-10-14 10:46:18 -0500

    update lsp from lsp-installer to mason

---
+++ .config/nvim/lua/user/lsp/installer.lua
@@ -0,0 +1,53 @@
+local status_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not status_ok then
+	return
+end
+
+require("mason").setup()
+
+mason_lspconfig.setup {
+  ensure_installed = {
+    "solargraph",
+    "sumneko_lua",
+    "tsserver",
+    "esling",
+    "jsonls",
+    "yamlls",
+    "emmetls"
+  }
+}
+
+-- Register a handler that will be called for all installed servers.
+-- Alternatively, you may also register handlers on specific server instances instead (see example below).
+
+local opts = {
+  on_attach = require("user.lsp.handlers").on_attach,
+  capabilities = require("user.lsp.handlers").capabilities,
+}
+
+mason_lspconfig.setup_handlers {
+  function(server_name)
+    require('lspconfig')[server_name].setup(opts)
+  end,
+
+  ["jsonls"] = function ()
+	 	local jsonls_opts = require("user.lsp.settings.jsonls")
+	 	local server_opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+
+    require("lspconfig")["jsonls"].setup(server_opts)
+  end,
+
+  ["sumneko_lua"] = function ()
+	 	local sumneko_opts = require("user.lsp.settings.sumneko_lua")
+	 	local server_opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+
+    require("lspconfig")["sumneko_lua"].setup(server_opts)
+  end,
+
+  ["pyright"] = function ()
+	 	local pyright_opts = require("user.lsp.settings.pyright")
+	 	local server_opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+
+    require("lspconfig")["pyright"].setup(server_opts)
+  end
+}
--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -77,8 +77,9 @@ return packer.startup(function(use)
   use "L3MON4D3/LuaSnip" -- snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
   -- LSP
+  use "williamboman/mason.nvim"
+  use "williamboman/mason-lspconfig.nvim"
   use "neovim/nvim-lspconfig" -- enable LSP
-  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
   -- Telescope
   use "nvim-telescope/telescope.nvim"
